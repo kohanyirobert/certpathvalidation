7d772c3132b0459a2e6650d93b519e832367d318
Kohányi Róbert <kohanyi.robert@gmail.com>
2015-06-14 07:30:42 +0200

    Update some cmds so that more keys and csr are generated.

--- src/main/resources/05-generate-certificate-signing-requests.cmd
+++ src/main/resources/05-generate-certificate-signing-requests.cmd
@@ -7,3 +7,33 @@ keytool -certreq^
  -storepass changeit^
  -storetype %_storetype%^
  -v
+
+rem generate certficate signing request for server's public key to file
+keytool -certreq^
+ -alias server^
+ -file server.csr^
+ -keypass changeit^
+ -keystore server.%_storetype%^
+ -storepass changeit^
+ -storetype %_storetype%^
+ -v
+
+rem generate certficate signing request for client1's public key to file
+keytool -certreq^
+ -alias client1^
+ -file client1.csr^
+ -keypass changeit^
+ -keystore client1.%_storetype%^
+ -storepass changeit^
+ -storetype %_storetype%^
+ -v
+
+rem generate certficate signing request for client2's public key to file
+keytool -certreq^
+ -alias client2^
+ -file client2.csr^
+ -keypass changeit^
+ -keystore client2.%_storetype%^
+ -storepass changeit^
+ -storetype %_storetype%^
+ -v
